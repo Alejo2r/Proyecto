6a1f3204dbc977c2d46b0f4e5510cc37c3593205
Alejo2r <sadith.alejandro@gmail.com>
2024-09-09 22:07:07 -0600

    Gran avance

---
+++ src/pokedex/Pokedex.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pokedex;
+
+/**
+ *
+ * @author Admin
+ */
+public class Pokedex {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Poke pika = new Poke();
+       pika.setVisible(true);
+       pika.setLocationRelativeTo(null);
+    }
+    
+}
---
+++ src/pokedex/Pokemon.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pokedex;
+
+/**
+ *
+ * @author Admin
+ */
+public class Pokemon {
+    public int vida;
+    public int ataque;
+    public String tipo;
+    public String nombre;
+    public Pokemon(){
+        
+    }
+
+    public Pokemon(int vida, int ataque, String tipo, String nombre) {
+        this.vida = vida;
+        this.ataque = ataque;
+        this.tipo = tipo;
+        this.nombre = nombre;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
